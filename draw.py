ab26fc9cce43ab2c5ccec163879e431dde2cc4fd
unknown <knosmos@gmail.com>
2021-09-03 00:25:30 -0400

    fix transparent background bug

--- draw.py
+++ draw.py
@@ -16,15 +16,32 @@ def detectStart():
 
 def loadImage(filename):
     # loads cv2 image from file
-    img = cv2.imread(filename)
-    #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
+    if img.shape[2] == 4:
+        alpha_channel = img[:,:,3]
+        rgb_channels = img[:,:,:3]
+
+        # White Background Image
+        white_background_image = np.ones_like(rgb_channels, dtype=np.uint8) * 255
+
+        # Alpha factor
+        alpha_factor = alpha_channel[:,:,np.newaxis].astype(np.float32) / 255.0
+        alpha_factor = np.concatenate((alpha_factor,alpha_factor,alpha_factor), axis=2)
+
+        # Transparent Image Rendered on White Background
+        base = rgb_channels.astype(np.float32) * alpha_factor
+        white = white_background_image.astype(np.float32) * (1 - alpha_factor)
+        final_image = base + white
+    else:
+        final_image = img
+
     # resize
     width = 100
     img_w = width
     img_h = int(width/img.shape[1] * img.shape[0])
+    final_image = cv2.resize(final_image, (img_w, img_h), interpolation = cv2.INTER_AREA)
 
-    img = cv2.resize(img, (img_w, img_h), interpolation = cv2.INTER_AREA)
-    return img
+    return final_image.astype(np.uint8)
 
 def rgb_to_hsv(color):
     arr = np.uint8([[color]])
@@ -271,7 +288,7 @@ def draw():
         
         usedpolygons.add(tuple(polygon))
         
-        if (polygon[0][0]-polygon[-1][0])**2 + (polygon[0][1]-polygon[-1][1])**2 > 300:
+        if (polygon[0][0]-polygon[-1][0])**2 + (polygon[0][1]-polygon[-1][1])**2 > 10000:
             prev_point = polygon[0]
             fill_poly = False
         else:
